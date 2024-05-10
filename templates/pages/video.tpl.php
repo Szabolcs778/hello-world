88e3df2a8102b066d5d1e348aaf88b8089db764e
Szabolcs778 <59250242+Szabolcs778@users.noreply.github.com>
2024-05-10 21:19:55 +0200

    Add files via upload
    Felmásolom a config.inc.php valami merge hiba volt.
    Megcsináltam a videó menüt is ! és töltöttem fel videót.

---
+++ templates/pages/video.tpl.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row">
+        <!-- YouTube videó -->
+        <div class="col-md-6">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/xeueZ4YUH4o" title="Autókereskedõk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+        <!-- Helyi videó -->
+        <div class="col-md-6">
+            <div class="embed-responsive embed-responsive-16by9">
+                <video class="embed-responsive-item" controls>
+                    <source src="video/video.mp4" type="video/mp4">
+                    A böngészõd nem támogatja a videó lejátszást.
+                </video>
+            </div>
+        </div>
+    </div>
+</div>
